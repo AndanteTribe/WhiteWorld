ad7951efb2175c6d696b2df15c429ee9e6826cc5
nattohikoki <nattohikoki@gmail.com>
2025-08-05 21:07:42 +0900

    feat: Added a button to shift to "Opening" scene to DebugViewer.cs

--- src/WhiteWorld.Unity/Assets/Scripts/Runtime/AppMain/DebugViewer.cs
+++ src/WhiteWorld.Unity/Assets/Scripts/Runtime/AppMain/DebugViewer.cs
@@ -1,6 +1,7 @@
 ﻿#if ENABLE_DEBUGTOOLKIT
 
 using DebugToolkit;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using VContainer.Unity;
 
@@ -11,12 +12,24 @@ namespace WhiteWorld.AppMain
         protected override VisualElement CreateViewGUI()
         {
             var root = base.CreateViewGUI();
-            
+
             var firstWindow = root.AddWindow("デバッグメニュー");
             firstWindow.AddProfileInfoLabel();
-            
+
+            // OpeningScene用のウィンドウを作製.
+            var openingWindow = root.AddWindow("OpeningScene");
+            openingWindow.style.flexDirection = FlexDirection.Column;
+            // Openingシーンへ遷移するボタンを作成.
+            var buttonToOpening = new Button();
+            buttonToOpening.text = "Shift to Opening Scene";
+            buttonToOpening.clicked += () => ShiftScene("Opening");
+            openingWindow.Add(buttonToOpening);
+
+
             return root;
         }
+
+        private void ShiftScene(string sceneName) => SceneManager.LoadScene(sceneName);
     }
 }
 
