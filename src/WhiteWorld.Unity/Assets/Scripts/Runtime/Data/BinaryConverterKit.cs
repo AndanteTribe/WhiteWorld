0f69b0d0d1c12d1a2bc58c5c973693acc08befb5
庄子 <>
2025-08-05 23:06:36 +0900

    feat: csvファイルからC#にしたデータをバイナリに変換する機能の実装

---
+++ src/WhiteWorld.Unity/Assets/Scripts/Runtime/Data/BinaryConverterKit.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using UnityEngine;
+using AndanteTribe.Utils.Unity;
+using MasterMemory;
+using UnityEditor;
+
+namespace WhiteWorld.Data.Runtime.Data
+{
+    /// <summary>
+    /// csvファイルをバイナリに変換するクラス
+    /// </summary>
+    public class BinaryConverterKit : MonoBehaviour
+    {
+        /// <summary>
+        /// 対象のcsvファイルのパス
+        /// </summary>
+        [SerializeField] private string _csvFilePath;
+
+        /// <summary>
+        /// 変換後のクラス名
+        /// </summary>
+        [SerializeField] private string _convertClassName;
+
+        /// <summary>
+        /// バイナリファイルの名前
+        /// </summary>
+        [SerializeField] private string _writeBinaryFileName;
+
+        private const string NameSpacePath = "WhiteWorld.Data.Runtime.Data.";
+        private const string WriteBinaryPath = "Assets/Downloads/BynaryData/";
+
+        [Button]
+        public void BuildBinaryData()
+        {
+            var type = Type.GetType($"{NameSpacePath}{_convertClassName}");
+            var csvText = File.ReadAllText(_csvFilePath);
+
+            if (type == null)
+            {
+                throw new ApplicationException("Typeが見つかりませんでした");
+            }
+
+            var deserialize = CsvTranslator.Deserialize(csvText, type);
+            var builder = new DatabaseBuilder();
+
+            builder.AnyAppend(deserialize);
+            var bytes = builder.Build();
+            File.WriteAllBytes($"{WriteBinaryPath}{_writeBinaryFileName}.bytes",bytes);
+
+            AssetDatabase.Refresh();
+
+            Debug.Log($"csvText:{csvText}, Type:{type}, bytes: {bytes}");
+        }
+    }
+}
---
+++ src/WhiteWorld.Unity/Assets/Scripts/Runtime/Data/CsvTranslator.cs
@@ -0,0 +1,77 @@
+using System;
+using Csv;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using MasterMemory;
+using UnityEditor;
+
+namespace WhiteWorld.Data.Runtime.Data
+{
+    public static class CsvTranslator
+    {
+        /// <summary>
+        /// 汎用デシアライザー
+        /// </summary>
+        /// <param name="csvText"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object Deserialize(string csvText, Type type)
+        {
+            // Deserialize<T> メソッドをリフレクションで取得
+            var deserializeMethod = typeof(CsvSerializer).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .FirstOrDefault(m =>
+                    m.Name == "Deserialize" &&
+                    m.IsGenericMethodDefinition &&
+                    m.GetParameters().Length >= 1 &&
+                    m.GetParameters()[0].ParameterType == typeof(string));
+
+            // 指定された型でジェネリックメソッドを作成
+            var genericDeserializeMethod = deserializeMethod?.MakeGenericMethod(type);
+
+            // ジェネリックメソッドをCSVテキストで実行
+            return genericDeserializeMethod!.Invoke(null, new object[] { csvText, null });
+        }
+
+        /// <summary>
+        /// 汎用アペンド
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DatabaseBuilder AnyAppend(this DatabaseBuilder builder, object data)
+        {
+            if (data is IEnumerable)
+            {
+                // 配列の要素の型を取得
+                var elementType = data.GetType().GetElementType();
+
+                if (elementType != null)
+                {
+                    // IEnumerable<elementType> 型を作成
+                    var enumerableType = typeof(IEnumerable<>).MakeGenericType(elementType);
+
+                    // DatabaseBuilder の CustomAppend メソッド（ジェネリック）を取得
+                    var appendMethod = typeof(DatabaseBuilder).GetMethod(
+                        "Append",
+                        new Type[] { enumerableType } // 作成した IEnumerable<elementType> 型を指定
+                    );
+
+                    // CustomAppend メソッドを実行
+                    appendMethod?.Invoke(builder, new object[] { data });
+                }
+                else
+                {
+                    throw new Exception("デシリアライズされたデータは配列ではありません");
+                }
+            }
+            else
+            {
+                throw new Exception("デシリアライズされたデータはコレクションではありません");
+            }
+
+            return builder;
+        }
+    }
+}
