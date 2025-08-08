abc3513f4ff8f40ab507a3ae4f73746604eb35b4
庄子 <>
2025-08-08 21:32:18 +0900

    fix: コンフリクト解消

--- src/WhiteWorld.Unity/Assets/Scripts/Runtime/AppMain/SystemLifetimeScope.cs
+++ src/WhiteWorld.Unity/Assets/Scripts/Runtime/AppMain/SystemLifetimeScope.cs
@@ -1,9 +1,8 @@
 using AndanteTribe.Utils.Unity.VContainer;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
-using WhiteWorld.Data.Runtime.Data;
 using WhiteWorld.Domain;
-using WhiteWorld.Domain.Entity.Runtime.Domain.Entity;
 using WhiteWorld.Presentation;
 
 namespace WhiteWorld.AppMain
@@ -13,20 +12,14 @@ namespace WhiteWorld.AppMain
     /// </summary>
     public sealed class SystemLifetimeScope : LifetimeScopeBase
     {
+        [SerializeField] private GameObject _tvPrefab;
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.RegisterEntryPoint<DebugViewer>();
             builder.Register<ISceneController, DefaultSceneController>(Lifetime.Singleton);
 
-            builder
-                .Register<IMasterDataRepository<DummyModel>, MasterDataRepository<DummyModel>>(Lifetime.Singleton)
-                .WithParameter("binaryPath","DummyText")
-                .WithParameter("tableName","DummyData");
-
-            builder
-                .Register<IMasterDataRepository<KeywordModel>, KeywordModelRepository>(Lifetime.Singleton)
-                .WithParameter("binaryPath", "Keyword")
-                .WithParameter("tableName", "KeywordData");
+#if ENABLE_DEBUGTOOLKIT
+            builder.RegisterEntryPoint<DebugViewer>().WithParameter(_tvPrefab);
+#endif
         }
     }
 }
