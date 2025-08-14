06d9f3efcf75a1d4ccc6c2bc328a27fe1155d514
庄子 <>
2025-08-14 23:42:46 +0900

    fix: 記憶のかけらマスを踏んだときにエラーになるバグの解消

--- src/WhiteWorld.Unity/Assets/Scripts/Runtime/Presentation/TextAnimationPresenter.cs
+++ src/WhiteWorld.Unity/Assets/Scripts/Runtime/Presentation/TextAnimationPresenter.cs
@@ -13,13 +13,15 @@ namespace WhiteWorld.Presentation.Runtime
         private TextAnimator _textAnimator;
 
         private ReadOnlyMemory<KeywordModel> _data;
+        private ReadOnlyMemory<DummyModel> _dummyModel;
         private AutoResetUniTaskCompletionSource _onFinish;
 
         [Inject]
-        public void Initialize(ReadOnlyMemory<KeywordModel> data, AutoResetUniTaskCompletionSource onFinish)
+        public void Initialize(ReadOnlyMemory<KeywordModel> data, ReadOnlyMemory<DummyModel> dummy, AutoResetUniTaskCompletionSource onFinish)
         {
             _data = data;
             _onFinish = onFinish;
+            _dummyModel = dummy;
         }
 
         private async UniTaskVoid Start()
@@ -27,8 +29,7 @@ namespace WhiteWorld.Presentation.Runtime
             var r = UnityEngine.Random.Range(0, _data.Length);
             var data = _data.Span[r];
             var keyword = data.KeywordText;
-            var dummyText = data.DummyData;
-            var dummy = dummyText.AsValueEnumerable()
+            var dummy = _dummyModel.AsValueEnumerable()
                 .Select(x => x.DummyText)
                 .ToArray();
 
